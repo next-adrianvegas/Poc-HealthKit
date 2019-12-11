f38c532e60380b822b4824e388eb53875456d267
adrian.vegas <adrian.vegas@bbva.com>
2019-12-11 19:42:26 +0100

    Create project for apple watch : workoutApp

---
+++ code_examples/HealthKitWorkoutApp/HealthKitWorkoutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitWorkoutApp
+//
+//  Created by adrian.vegas on 11/12/2019.
+//  Copyright © 2019 bbva.next. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
